3aa6634023a3ae960038dfa4eef88e923e7a280b
Alexandre Delaboneta © <alexdelaboneta@gmail.com>
2024-08-18 18:09:57 -0300

    first commit

---
+++ src/pages/historico/historico.jsx
@@ -0,0 +1,35 @@
+import Navbar from "../../components/navbar/navbar.jsx";
+import "./historico.css";
+import {pedidos} from "../../dados.js";
+
+function Historico(){
+    
+    return <>
+        <Navbar showMenu={true} />
+
+        <div className="container">
+            <div className="titulo text-center">
+                <h1>Histórico de Pedidos</h1>       
+            </div>
+
+            <div className="box-pedido">
+                <table className="table">
+                    {
+                        pedidos.map( function(ped){
+                            return <tr key={ped.id_pedido}>
+                                    <td><strong>Pedido {ped.id_pedido}</strong></td>
+                                    <td className="text-light">{ped.dt}</td>
+                                    <td className="text-red">{new Intl.NumberFormat('pt-BR', 
+                                                    {style: 'currency', currency: "BRL"}).format(ped.total)}</td>
+                                </tr>
+                        })                        
+                    }
+                </table>
+            </div>
+        </div>
+        
+       
+    </>
+}
+
+export default Historico;
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,31 @@
+import Navbar from "../../components/navbar/navbar.jsx";
+import ProdutoVitrine from "../../components/produto-vitrine/produto-vitrine.jsx";
+import {produtos} from "../../dados.js";
+
+function Home(){
+    return <>
+        <Navbar showMenu={true} />
+
+        <div className="container">
+            <div className="titulo text-center">
+                <h1>Nosso Cardápio</h1>
+                <p className="subtitulo">Clique em adicionar para colocar os produtos na sacola de compras. Se preferir, você pode pedir pelo WhatsApp: (11) 94785-2140</p>
+            </div>
+        </div>
+        
+        <div className="text-center">
+            {
+                produtos.map(function(prod){
+                    return <ProdutoVitrine key={prod.id}
+                                           id={prod.id}
+                                           nome={prod.nome}
+                                           descricao={prod.descricao}
+                                           preco={prod.preco}
+                                           foto={prod.foto}  />
+                })                
+            }
+        </div>
+    </>
+}
+
+export default Home;
